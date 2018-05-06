dbe6a04cf6e5179a3e380b0f8dcd1f6d01039b7f
inpercima <inpercima@gmail.com>
2018-05-06 16:25:46 +0200

    Update pom.xml for java apps
    * close #13
    * update Application.java and logback.xml

--- endpoints/java/Application.java
+++ endpoints/java/Application.java
@@ -1,4 +1,4 @@
-package PROJECTDATA_PACKAGEPATH;
+package net.inpercima.swaaplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
 /**
- * @author PROJECTDATA_AUTHOR
+ * @author Marcel Jänicke
  */
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
