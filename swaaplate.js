93cc7efaa28e45a5156804d83f5024d9f54b0f5d
inpercima <inpercima@gmail.com>
2020-02-11 09:46:22 +0100

    Update console outputs

--- swaaplate.js
+++ swaaplate.js
@@ -15,14 +15,14 @@ function init() {
   lightjs.info('initialize swaaplate');
   var args = process.argv.slice(2);
   if (args.length === 1) {
-    lightjs.info('One parameter was found, use as absolute workspace path.');
+    lightjs.info('one parameter was found, use as absolute workspace path');
     swProject.create(args[0]);
   } else if (args.length === 0) {
-    lightjs.error('No absolute path was specified for the project!');
+    lightjs.error('no absolute path was specified for the project');
   } else if (args.length === 2 && args[0] === '-u') {
-    lightjs.info('Two parameters were found, use first as updating the project, second as absolute workspace path.');
+    lightjs.info('two parameters were found, use first as updating the project, second as absolute workspace path');
     swProject.update(args[1]);
   } else {
-    lightjs.error('Too many parameters were specified. Only one parameter for the workspace path of the project is allowed!');
+    lightjs.error('too many parameters were specified. Only one parameter for the workspace path of the project is allowed');
   }
 }
