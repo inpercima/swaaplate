ffac7b1020c5240be4d16f27d39bc40410282c4c
inpercima <inpercima@gmail.com>
2020-08-22 12:42:42 +0200

    Update dependencies and configuration

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = Object.freeze({
-  ANGULAR_CDK: '~9.2.4',
-  ANGULAR_CLI: '9.1.7',
-  ANGULAR_FLEX: '~9.0.0-beta.31',
+  ANGULAR_CDK: '~10.1.3',
+  ANGULAR_CLI: '10.0.7',
+  ANGULAR_FLEX: '~10.0.0-beta.32',
   ANGULAR_JWT: '~4.0.0',
   APACHE: '2.4',
   COPY_WEBPACK_PLUGIN: '4.6.0',
-  CUSTOM_WEBPACK: '9.1.0',
+  CUSTOM_WEBPACK: '10.0.0',
   DOCKER_COMPOSE: '1.25.0',
   DOCKER: '19.03.5',
   JDK: '11',
@@ -17,6 +17,6 @@ module.exports = Object.freeze({
   NODE: '12.16.1',
   NPM: '6.13.4',
   PHP: '7.3',
-  SPRING_BOOT: '2.3.0.RELEASE',
+  SPRING_BOOT: '2.3.3.RELEASE',
   YARN: '1.22.4',
 });
