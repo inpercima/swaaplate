0b1c94f05aa8433abf84379c5f3f4ac0c6449dfa
inpercima <inpercima@gmail.com>
2020-09-25 21:22:47 +0200

    Update version for project and templates

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = Object.freeze({
-  ANGULAR_CDK: '~10.2.1',
-  ANGULAR_CLI: '10.1.1',
+  ANGULAR_CDK: '~10.2.2',
+  ANGULAR_CLI: '10.1.3',
   ANGULAR_FLEX: '~10.0.0-beta.32',
   ANGULAR_JWT: '~5.0.1',
   APACHE: '2.4',
   COPY_WEBPACK_PLUGIN: '6.0.2',
-  CUSTOM_WEBPACK: '10.0.0',
+  CUSTOM_WEBPACK: '10.0.1',
   DOCKER_COMPOSE: '1.26.0',
   DOCKER: '19.03.12',
   JDK: '14',
@@ -18,5 +18,5 @@ module.exports = Object.freeze({
   NPM: '6.13.4',
   PHP: '7.3',
   SPRING_BOOT: '2.3.3.RELEASE',
-  YARN: '1.22.4',
+  YARN: '1.22.5',
 });
