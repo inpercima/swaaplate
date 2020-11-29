cef157e3d2f4f484576dcd9ebadcf7d0fe234bfc
inpercima <inpercima@gmail.com>
2020-11-29 20:16:44 +0100

    Update angular-cli to 11.0.2

--- website/README.md
+++ website/README.md
@@ -6,18 +6,18 @@
 
 The generation tool for swaaplate to create the configuration (json) file.
 
-This project was generated with [swaaplate](https://github.com/inpercima/swaaplate) version 2.0.1-SNAPSHOT.
+This project was generated with [swaaplate](https://github.com/inpercima/swaaplate) version 2.1.0.
 
 ## Prerequisites
 
 ### Angular CLI
 
-* `angular-cli 10.1.3` or higher
+* `angular-cli 11.0.2` or higher
 
 ### Node, npm or yarn
 
-* `node 12.16.1` or higher in combination with
-  * `npm 6.13.4` or higher or
+* `node 12.19.0` or higher in combination with
+  * `npm 6.14.8` or higher or
   * `yarn 1.22.5` or higher, used in this repository
 
 ## Dependency check
@@ -26,13 +26,14 @@ Some libraries could not be updated b/c of peer dependencies or knowing issues.
 
 | library    | current version | wanted version | reason |
 | ---------- | --------------- | -------------- | ------ |
-| zone.js | 0.10.3 | 0.11.1 | "@angular/core@10.1.4" has incorrect peer dependency "zone.js@~0.10.3" |
+| typescript | 4.0.5           | 4.1.2          | @angular-devkit/build-angular@0.1100.2" has incorrect peer dependency "typescript@~4.0.0" |
+| zone.js    | 0.10.3          | 0.11.3         | @angular/core@11.0.2" has incorrect peer dependency "zone.js@~0.10.3" |
 
 ## Getting started
 
 ```bash
 # clone project
-git clone https://github.com/inpercima/swaaplate
+git clone https://github.com/inpercima/swaaplate/
 cd swaaplate
 
 # install tools and frontend dependencies
--- website/src/app/features/generator/generator.component.ts
+++ website/src/app/features/generator/generator.component.ts
@@ -1,47 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'swg-generator',
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css'],
 })
-export class GeneratorComponent implements OnInit {
-
-  generatorForm: FormGroup;
+export class GeneratorComponent {
 
   backends: string[] = [ 'Javascript', 'Java', 'Kotlin', 'php' ];
   themes: string[] = [ 'indigo-pink', 'deeppurple-amber', 'pink-bluegrey', 'purple-green', 'custom-light', 'custom-dark' ];
 
-  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.generatorForm = this.formBuilder.group({
-      name: ['hello-world', Validators.required],
-      title: ['Hello World', Validators.required],
-      author:  ['', Validators.required],
-      mail:  [''],
-      description: ['An example app.', Validators.required],
-      theme: [this.themes[0], Validators.required],
-      buildDir: ['dist', Validators.required],
-      ghUser: [''],
-      language: ['en', Validators.required],
-      prefix:  ['hw', Validators.required],
-      useDocker: [false],
-      useMITLicense: [true],
-      useMock: [false],
-      useSecurity: [false],
-      modRewriteIndex: [false],
-      useGoogleFonts: [true],
-      installDependencies: [true],
-      useYarn: ['yarn'],
-      backend: [this.backends[0]],
-      groupId: ['org.example.hello_world'],
-      management: ['maven'],
-      modRewritePhpExtension: [true],
-      serverAsApi: [true],
-    });
-  }
+  generatorForm = this.formBuilder.group({
+    name: ['hello-world', Validators.required],
+    title: ['Hello World', Validators.required],
+    author:  ['', Validators.required],
+    mail:  [''],
+    description: ['An example app.', Validators.required],
+    theme: [this.themes[0], Validators.required],
+    buildDir: ['dist', Validators.required],
+    ghUser: [''],
+    language: ['en', Validators.required],
+    prefix:  ['hw', Validators.required],
+    useDocker: [false],
+    useMITLicense: [true],
+    useMock: [false],
+    useSecurity: [false],
+    modRewriteIndex: [false],
+    useGoogleFonts: [true],
+    installDependencies: [true],
+    useYarn: ['yarn'],
+    backend: [this.backends[0]],
+    groupId: ['org.example.hello_world'],
+    management: ['maven'],
+    modRewritePhpExtension: [true],
+    serverAsApi: [true],
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   checkBackend(backends: string[]): boolean {
     return backends.includes(this.generatorForm.value.backend);
