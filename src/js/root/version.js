660c5bc0cd91ee78e78d14881090cdce60998352
inpercima <inpercima@gmail.com>
2021-11-15 07:16:29 +0100

    Update dependencies for applications

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -1,22 +1,29 @@
 'use strict';
 
 module.exports = Object.freeze({
-  ANGULAR_CDK_MATERIAL: '12.1.1',
-  ANGULAR_CLI: '12.1.1',
-  ANGULAR_FLEX: '12.0.0-beta.34',
+  // https://www.npmjs.com/package/@angular/material
+  ANGULAR_CDK_MATERIAL: '13.0.0',
+  // https://www.npmjs.com/package/@angular/cli
+  ANGULAR_CLI: '13.0.2',
+  // https://www.npmjs.com/package/@angular/flex-layout
+  ANGULAR_FLEX: '12.0.0-beta.35',
+  // https://www.npmjs.com/package/@auth0/angular-jwt
   ANGULAR_JWT: '5.0.2',
   APACHE: '2.4',
-  COPY_WEBPACK_PLUGIN: '9.0.0',
-  CUSTOM_WEBPACK: '12.1.0',
+  COPY_WEBPACK_PLUGIN: '9.0.1',
+  CUSTOM_WEBPACK: '13.0.0',
   DOCKER_COMPOSE: '1.26.0',
   DOCKER: '19.03.12',
   JDK: '16',
-  JSON_SERVER: '0.16.3',
+  // https://www.npmjs.com/package/json-server
+  JSON_SERVER: '0.17.0',
+  // https://www.npmjs.com/package/jsonwebtoken
   JSONWEBTOKEN: '8.5.1',
+  // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
   MAVEN_JAR_PLUGIN: '3.2.0',
-  NODE: '14.16.1',
-  NPM: '6.14.12',
+  NODE: '16.13.0',
+  NPM: '8.1.0',
   PHP: '7.3',
-  SPRING_BOOT: '2.5.1',
-  YARN: '1.22.5',
+  SPRING_BOOT: '2.5.6',
+  YARN: '1.22.11',
 });
