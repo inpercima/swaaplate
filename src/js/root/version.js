88752cbabb98f3f9e472e3525771daa05ecc9b20
inpercima <inpercima@gmail.com>
2022-05-30 11:19:10 +0200

    Update angular-dependencies and spring-boot to 2.7.0

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -2,18 +2,18 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '13.3.2',
+  ANGULAR_CDK_MATERIAL: '13.3.8',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '13.3.2',
+  ANGULAR_CLI: '13.3.7',
   // https://www.npmjs.com/package/@angular-eslint/schematics
-  ANGULAR_ESLINT_SCHEMATICS: '13.2.0',
+  ANGULAR_ESLINT_SCHEMATICS: '13.2.1',
   // https://www.npmjs.com/package/@angular/flex-layout
   ANGULAR_FLEX: '13.0.0-beta.38',
   // https://www.npmjs.com/package/@auth0/angular-jwt
   ANGULAR_JWT: '5.0.2',
   APACHE: '2.4',
   // https://www.npmjs.com/package/copy-webpack-plugin
-  COPY_WEBPACK_PLUGIN: '10.2.4',
+  COPY_WEBPACK_PLUGIN: '11.0.0',
   // https://www.npmjs.com/package/@angular-builders/custom-webpack
   CUSTOM_WEBPACK: '13.1.0',
   DOCKER_COMPOSE: '1.27.0',
@@ -29,6 +29,6 @@ module.exports = Object.freeze({
   NPM: '8.1.0',
   PHP: '7.4',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-  SPRING_BOOT: '2.6.6',
+  SPRING_BOOT: '2.7.0',
   YARN: '1.22.17',
 });
