c36426ab4f81293b145a8833f14cc6523f4c9e90
inpercima <inpercima@gmail.com>
2022-06-07 10:40:13 +0200

    Update @angular to 14.0.0 and npm to 8.12.1

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -2,9 +2,9 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '13.3.8',
+  ANGULAR_CDK_MATERIAL: '14.0.0',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '13.3.7',
+  ANGULAR_CLI: '14.0.0',
   // https://www.npmjs.com/package/@angular-eslint/schematics
   ANGULAR_ESLINT_SCHEMATICS: '13.2.1',
   // https://www.npmjs.com/package/@angular/flex-layout
@@ -26,7 +26,7 @@ module.exports = Object.freeze({
   // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
   MAVEN_JAR_PLUGIN: '3.2.2',
   NODE: '16.13.0',
-  NPM: '8.1.0',
+  NPM: '8.12.1',
   PHP: '7.4',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
   SPRING_BOOT: '2.7.0',
