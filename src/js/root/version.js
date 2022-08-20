6f5a7c67015cbba71e46b1e97a94cacb80c1b65b
inpercima <inpercima@gmail.com>
2022-08-20 14:21:54 +0200

    Update several versions

--- src/js/root/version.js
+++ src/js/root/version.js
@@ -2,9 +2,9 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '14.1.0',
+  ANGULAR_CDK_MATERIAL: '14.1.3',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '14.1.0',
+  ANGULAR_CLI: '14.1.3',
   // https://www.npmjs.com/package/@angular-eslint/schematics
   ANGULAR_ESLINT_SCHEMATICS: '14.0.2',
   // https://www.npmjs.com/package/@angular/flex-layout
@@ -13,13 +13,13 @@ module.exports = Object.freeze({
   ANGULAR_JWT: '5.0.2',
   APACHE: '2.4',
   // https://www.npmjs.com/package/@cypress/schematic
-  CYPRESS_SCHEMATIC: '2.0.0',
+  CYPRESS_SCHEMATIC: '2.0.3',
   // https://www.npmjs.com/package/copy-webpack-plugin
   COPY_WEBPACK_PLUGIN: '11.0.0',
   // https://www.npmjs.com/package/@angular-builders/custom-webpack
   CUSTOM_WEBPACK: '14.0.0',
-  DOCKER_COMPOSE: '2.3.3',
-  DOCKER: '20.10.14',
+  DOCKER_COMPOSE: '2.10.0',
+  DOCKER: '20.10.17',
   JDK: '17',
   // https://www.npmjs.com/package/json-server
   JSON_SERVER: '0.17.0',
@@ -28,9 +28,9 @@ module.exports = Object.freeze({
   // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
   MAVEN_JAR_PLUGIN: '3.2.2',
   NODE: '16.16.0',
-  NPM: '8.12.2',
+  NPM: '8.18.0',
   PHP: '7.4',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-  SPRING_BOOT: '2.7.2',
+  SPRING_BOOT: '2.7.3',
   YARN: '1.22.19',
 });
