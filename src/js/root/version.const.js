1f34aaa706fb18ad6c11fe7261efac73cdffccaa
inpercima <inpercima@gmail.com>
2022-12-29 21:38:43 +0100

    Update dependencies for projects

--- src/js/root/version.const.js
+++ src/js/root/version.const.js
@@ -2,13 +2,13 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '15.0.2',
+  ANGULAR_CDK_MATERIAL: '15.0.3',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '15.0.3',
+  ANGULAR_CLI: '15.0.4',
   // https://www.npmjs.com/package/@angular-eslint/schematics
   ANGULAR_ESLINT_SCHEMATICS: '15.1.0',
   // https://www.npmjs.com/package/@auth0/angular-jwt
-  ANGULAR_JWT: '5.1.0',
+  ANGULAR_JWT: '5.1.2',
   APACHE: '2.4',
   // https://www.npmjs.com/package/@cypress/schematic
   CYPRESS_SCHEMATIC: '2.4.0',
@@ -18,7 +18,7 @@ module.exports = Object.freeze({
   // https://www.npmjs.com/package/json-server
   JSON_SERVER: '0.17.1',
   // https://www.npmjs.com/package/jsonwebtoken
-  JSONWEBTOKEN: '8.5.1',
+  JSONWEBTOKEN: '9.0.0',
   // https://www.npmjs.com/package/material-icons
   MATERIAL_ICONS: '1.13.1',
   // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
@@ -27,6 +27,6 @@ module.exports = Object.freeze({
   NPM: '8.19.2',
   PHP: '7.4',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-  SPRING_BOOT: '2.7.5',
+  SPRING_BOOT: '3.0.1',
   YARN: '1.22.19',
 });
