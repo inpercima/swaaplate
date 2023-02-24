f21a275a70582771e65fa6b9c5d7ef8d87182162
Marcel Jänicke <inpercima@gmail.com>
2023-02-24 15:02:49 +0100

    Update versions

--- src/js/root/version.const.js
+++ src/js/root/version.const.js
@@ -2,9 +2,9 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '15.1.4',
+  ANGULAR_CDK_MATERIAL: '15.2.0',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '15.1.5',
+  ANGULAR_CLI: '15.2.0',
   // https://www.npmjs.com/package/@angular-eslint/schematics
   ANGULAR_ESLINT_SCHEMATICS: '15.2.1',
   // https://www.npmjs.com/package/@auth0/angular-jwt
@@ -12,21 +12,21 @@ module.exports = Object.freeze({
   APACHE: '2.4',
   // https://www.npmjs.com/package/@cypress/schematic
   CYPRESS_SCHEMATIC: '2.5.0',
-  DOCKER_COMPOSE: '2.11.0',
-  DOCKER: '20.10.18',
+  DOCKER_COMPOSE: '2.16.0',
+  DOCKER: '23.0.1',
   JDK: '17',
   // https://www.npmjs.com/package/json-server
-  JSON_SERVER: '0.17.1',
+  JSON_SERVER: '0.17.2',
   // https://www.npmjs.com/package/jsonwebtoken
   JSONWEBTOKEN: '9.0.0',
   // https://www.npmjs.com/package/material-icons
   MATERIAL_ICONS: '1.13.1',
   // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
   MAVEN_JAR_PLUGIN: '3.3.0',
-  NODE: '18.10.0',
-  NPM: '8.19.2',
-  PHP: '7.4',
+  NODE: '18.14.2',
+  NPM: '8.19.3',
+  PHP: '8.0',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-  SPRING_BOOT: '3.0.2',
+  SPRING_BOOT: '3.0.3',
   YARN: '1.22.19',
 });
