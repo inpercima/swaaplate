941f2f60df2aae25c4f66ec539f2bd3f9cc73619
Marcel Hendrich <inpercima@gmail.com>
2023-12-31 14:24:02 +0100

    chore(): Update agnular to 17.0.8 and its dependencies

--- src/js/root/version.const.js
+++ src/js/root/version.const.js
@@ -2,31 +2,31 @@
 
 module.exports = Object.freeze({
   // https://www.npmjs.com/package/@angular/material
-  ANGULAR_CDK_MATERIAL: '16.1.3',
+  ANGULAR_CDK_MATERIAL: '17.0.4',
   // https://www.npmjs.com/package/@angular/cli
-  ANGULAR_CLI: '16.1.3',
+  ANGULAR_CLI: '17.0.8',
   // https://www.npmjs.com/package/@angular-eslint/schematics
-  ANGULAR_ESLINT_SCHEMATICS: '16.0.3',
+  ANGULAR_ESLINT_SCHEMATICS: '17.1.1',
   // https://www.npmjs.com/package/@auth0/angular-jwt
   ANGULAR_JWT: '5.1.2',
   APACHE: '2.4',
   // https://www.npmjs.com/package/@cypress/schematic
-  CYPRESS_SCHEMATIC: '2.5.0',
+  CYPRESS_SCHEMATIC: '2.5.1',
   DOCKER_COMPOSE: '2.16.0',
   DOCKER: '23.0.1',
   JDK: '17',
   // https://www.npmjs.com/package/json-server
-  JSON_SERVER: '0.17.3',
+  JSON_SERVER: '0.17.4',
   // https://www.npmjs.com/package/jsonwebtoken
-  JSONWEBTOKEN: '9.0.0',
+  JSONWEBTOKEN: '9.0.2',
   // https://www.npmjs.com/package/material-icons
-  MATERIAL_ICONS: '1.13.8',
+  MATERIAL_ICONS: '1.13.12',
   // https://maven.apache.org/plugins/maven-jar-plugin/download.cgi
   MAVEN_JAR_PLUGIN: '3.3.0',
-  NODE: '18.16.0',
-  NPM: '9.5.1',
+  NODE: '20.9.0',
+  NPM: '10.1.0',
   PHP: '8.0',
   // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-  SPRING_BOOT: '3.1.1',
+  SPRING_BOOT: '3.2.1',
   YARN: '1.22.19',
 });
